488d4ed4aa12c35e8165d6541d7a88c461c041c8
Oketa Fred <oketafred@gmail.com>
2021-02-17 15:29:03 +0300

    Added Subscribe Option

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,24 +35,36 @@
                 </div>
             </div>
 
-            {{-- @include('layouts.navigation') --}}
+            <div class="container mx-auto px-4 mt-8">
+              {{-- @include('layouts.navigation') --}}
+              <form action="/email/subscribe" method="POST">
+                <div class="flex justify-between items-center flex-wrap md:flex-no-wrap bg-gray-500 mb-8 rounded py-3 px-3">
+                  <div class="flex-grow flex justify-end flex-col md:flex-row">
+                    <div class="md:px-2 md:self-center font-bold mb-1 md:mb-0 text-gray-200">Get an email of all new jobs</div> 
+                    <input type="text" name="email" placeholder="Your email address" class="px-4 py-2 rounded-sm md:mr-2 flex-grow focus:outline-none focus:shadow-outline mb-2 md:mb-0"> 
+                    <button type="submit" class="px-4 py-2 rounded-sm bg-red-500 text-white hover:bg-red-400 hover:text-white">Subscribe</button>
+                  </div>
+                </div>
+              </form>
+            </div>
 
-            <main>
-                <div class="mx-auto py-6 sm:px-6 lg:px-8">
+            <main class="container mx-auto px-4 mt-8">
+                <div class="mx-auto">
+                  
                   <!-- Replace with your content -->
-                  <div class="px-4 py-6 sm:px-0">
+                  <div class="py-6 sm:px-0">
                     <div class="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
                   </div>
-                  <div class="px-4 py-6 sm:px-0">
+                  <div class="py-6 sm:px-0">
                     <div class="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
                   </div>
-                  <div class="px-4 py-6 sm:px-0">
+                  <div class="py-6 sm:px-0">
                     <div class="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
                   </div>
-                  <div class="px-4 py-6 sm:px-0">
+                  <div class="py-6 sm:px-0">
                     <div class="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
                   </div>
-                  <div class="px-4 py-6 sm:px-0">
+                  <div class="py-6 sm:px-0">
                     <div class="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
                   </div>
                   <!-- /End replace -->
